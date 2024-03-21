b0f85e539a4a0b1fbd26a56677cf08f933a30543
TheETfinder <93601206+TheETfinder@users.noreply.github.com>
2024-03-21 11:53:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,62 @@
+import openmeteo_requests
+import yfinance as yf
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+import json
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 49.0094,
+	"longitude": 8.4044,
+	"hourly": ["temperature_2m", "rain"],
+	"timezone": "Europe/Berlin"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_rain = hourly.Variables(1).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["rain"] = hourly_rain
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
+
+msft = yf.Ticker("^GDAXI")
+info = msft.info
+print(msft)
+print(info)
+with open('data.json', 'w') as f:
+    json.dump(info, f)
+
+hourly_dataframe = hourly_dataframe.to_json()
+
+with open('wetter.json', 'w') as f:
+    json.dump(hourly_dataframe, f)
+
+
+
 
