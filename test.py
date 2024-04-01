d41777f96d9df14b52ba95917e90c0a63ece4fae
TheETfinder <93601206+TheETfinder@users.noreply.github.com>
2024-04-01 03:12:21 +0200

    Updated main.py

--- test.py
+++ test.py
@@ -16,7 +16,8 @@ url = "https://api.open-meteo.com/v1/forecast"
 params = {
 	"latitude": 49.054539,
 	"longitude": 8.399134,
-	"hourly": "temperature_2m",
+	"current": "temperature_2m",
+	"hourly": ["temperature_2m", "rain", "wind_speed_10m"],
 	"forecast_days": 1
 }
 responses = openmeteo.weather_api(url, params=params)
@@ -28,9 +29,18 @@ print(f"Elevation {response.Elevation()} m asl")
 print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
 print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
+# Current values. The order of variables needs to be the same as requested.
+current = response.Current()
+current_temperature_2m = current.Variables(0).Value()
+
+print(f"Current time {current.Time()}")
+print(f"Current temperature_2m {current_temperature_2m}")
+
 # Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
 hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_rain = hourly.Variables(1).ValuesAsNumpy()
+hourly_wind_speed_10m = hourly.Variables(2).ValuesAsNumpy()
 
 hourly_data = {"date": pd.date_range(
 	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
@@ -39,9 +49,12 @@ hourly_data = {"date": pd.date_range(
 	inclusive = "left"
 )}
 hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["rain"] = hourly_rain
+hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
 print(hourly_dataframe)
+
 
 
 # Inizialising the Stock we want the info for
