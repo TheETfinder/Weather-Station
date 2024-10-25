cb981c14d0aae3256a3e73b10e9e4107303e2b37
Etienne <TheETfinder@outlook.de>
2024-10-25 16:46:53 +0200

    V1.1.0
    It finally works. Everything is merged into one file.

--- datarequests.py
+++ datarequests.py
@@ -23,7 +23,7 @@ params = {
 	"longitude": 13.404772, 
 	"current": "temperature_2m",
 	"hourly": ["temperature_2m", "rain", "wind_speed_10m"],
-	"forecast_days": 5
+	"forecast_days": 2
 }
 responses = openmeteo.weather_api(url, params=params)
 
@@ -79,10 +79,6 @@ hourly_dataframe = hourly_dataframe.to_json()
 
 print(hourly_dataframe)
 
-with open("wetter.json", "w", encoding="utf-8") as weath:
-    json.dump(hourly_dataframe, weath , ensure_ascii=False, indent= 4, )
-    
-
 with open('data.json', mode= "r", encoding="utf-8") as stock:
     stock_data = json.load(stock)
 
@@ -91,17 +87,30 @@ current_price = stock_data["currentPrice"]
 
 # Printing the Current Price
 print("The current stock price for MSFT is", current_price, "USD")
-
-# Opening the Weather Data File and loading it 
+# Opening the Weather Data File and loading it  
 with open('wetter.json',mode="r", encoding="utf-8") as weather:
     weather_data = json.load(weather)
 
 # Getting the Current Temperatur
- #   temperatur = weather_data["temperature_2m"]
+    temperatur = weather_data["temperature_2m"]
+ 
+    current_temperatur = temperatur["19"]
 
-  #  current_temperatur = temperatur["13"]
+    print("The current Temperatur at 5 p.m is", current_temperatur)
+    
 
-  #  print("The current Temperatur at 1 p.m is", current_temperatur)
+    rain = weather_data["rain"]
+ 
+    current_rain = rain["17"]
+
+    print("The Rain at 5 p.m is", current_rain)
+   # current_price_json = current_price.to_json()
+
+    with open('searche_data.json', 'w') as data:
+        json.dump(current_price, data)
+
+    with open('weather.json', 'w') as data:
+        json.dump(current_temperatur, data)
 
 
 
